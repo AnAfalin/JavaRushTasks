0a8f4c6307ed1b65f3eb259aa545041d9b9a242e
Anastasia <an.afalin@yandex.ru>
2022-03-18 22:53:58 +0300

    add solution from Java Syntax tasks: 0601, 0602, 0603, 0604, 0605, 0606, 0607, 0608, 0609, 0610, 0611, 0612, 0613, 0614, 0615, 0616

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(int number){ }
+    public static void universalMethod(String name){ }
+    public static void universalMethod(int date, String city){ }
+    public static void universalMethod(int number, int date){ }
+    public static void universalMethod(int number, int date, String ciy){ }
+    public static void universalMethod(String firstName, String lastName){ }
+    public static void universalMethod(String firstName, String lastName, int year){ }
+    public static void universalMethod(double number){ }
+    public static void universalMethod(char element){ }
+    public static void universalMethod(char element, String title){ }
+
+}
