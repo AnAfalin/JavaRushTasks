17cba539cb76f7842dbaeb005b89c5cd7e9ae7ce
Anastasia <an.afalin@yandex.ru>
2022-03-19 12:13:11 +0300

    add solution from Java Syntax tasks: 0805

--- 1.JavaSyntax/src/com/javarush/task/pro/task08/task0805/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0805/Solution.java
@@ -10,37 +10,7 @@ public class Solution {
 
     public static void main(String[] args) {
         System.out.println("Заработаю ли я себе на BMW?");
-        MagicBall.getPrediction();
+        System.out.println(MagicBall.getPrediction());
     }
 
-    public static void getPrediction() {
-        Random r = new Random();
-        int x = r.nextInt(1);
-        switch (x) {
-            case (0) :
-                System.out.println( "Беспорно");
-                break;
-            case (1) :
-                System.out.println( "Определенно");
-                break;
-            case (2) :
-                System.out.println("Вероятнее всего");
-                break;
-            case (3) :
-                System.out.println("Хорошие перспективы");
-                break;
-            case (4) :
-                System.out.println("Спроси позже");
-                break;
-            case (5) :
-                System.out.println("Попробуй снова");
-                break;
-            case (6) :
-                System.out.println("Мой ответ - нет");
-                break;
-            case (7) :
-                System.out.println("Весьма сомнительно");
-                break;
-        }
-    }
 }
