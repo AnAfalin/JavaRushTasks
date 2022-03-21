478c88d7c880752bb88ea182ab6f49d2f8f8c5ba
Anastasia <an.afalin@yandex.ru>
2022-03-21 10:35:17 +0300

    add a solution from Java Syntax task 0918 (theme "StringBuilder")

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0918/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task09.task0918;
+
+/* 
+Поработаем со StringBuilder
+*/
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "Учиться, учиться и еще раз учиться! ";
+
+        System.out.println(addTo(string, new String[]{"Под ", "лежачий ", "камень ", "вода ", "не ", "течет"}));
+        System.out.println(replace(string, ", ", 16, 27));
+    }
+
+    public static StringBuilder addTo(String string, String[] strings) {
+        StringBuilder stringBuilder = new StringBuilder(string);
+        String tmp = "";
+        for (int i = 0; i < strings.length; i++) {
+            tmp += strings[i];
+        }
+        stringBuilder.append(tmp);
+        System.out.println(stringBuilder);
+        return stringBuilder;
+    }
+
+    public static StringBuilder replace(String string, String str, int start, int end) {
+        StringBuilder stringBuilder = new StringBuilder(string);
+        stringBuilder.replace(start, end, str);
+        return stringBuilder;
+    }
+}
